879788e199581ea0140a77784403fe4bb866f0e0
quantumdot <quantumdot@users.noreply.github.com>
2024-11-07 15:26:41 -0500

    remove import that is no longer valid

--- fptools/viz/__init__.py
+++ fptools/viz/__init__.py
@@ -1,2 +1,2 @@
 from .signal_collector import collect_signals, collect_signals_2event
-from .viz import plot_heatmap, plot_signal, aggregate_signals, sig_catplot
+from .viz import plot_heatmap, plot_signal, sig_catplot
