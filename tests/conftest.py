5f77b7fb4f93598d007b4447a5156ed790014094
Josh Thackray <thackray@rutgers.edu>
2025-02-07 11:31:07 -0500

    setup testing data

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+
+
+
+
+import requests
+import zipfile
+import io
+import os
+import glob
+
+
+def pytest_sessionstart(session):
+
+    test_data_link = r"https://rutgers.box.com/shared/static/pd6pl4ieo9je5ahh2f22z3t0yssxjern.zip"
+    dest = os.path.join(os.getcwd(), 'test_data')
+
+    # check if the data appears to be already in place
+    if len(glob.glob(os.path.join(dest, "*"))) <= 0:
+        print(f"Downloading test data and unpacking to \"{dest}\"")
+        r = requests.get(test_data_link)
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(dest)
+    else:
+        print("Test data appears to already be in place")
