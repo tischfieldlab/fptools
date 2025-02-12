62836820f01d69d367ce38461c75c48f2a8715c1
Josh Thackray <thackray@rutgers.edu>
2025-02-12 16:29:43 -0500

    add test for dynamic peak detection arg

--- tests/measure/test_peaks.py
+++ tests/measure/test_peaks.py
@@ -1,8 +1,15 @@
 
+import numpy as np
 
-
+from fptools.io import Session, Signal
 from fptools.measure import measure_peaks
 
 
 def test_measure_peaks(tdt_preprocessed_sessions):
     measure_peaks(tdt_preprocessed_sessions, signal="Dopamine")
+
+def test_measure_peaks_dynamic(tdt_preprocessed_sessions): 
+    def height_filter(session: Session, signal: Signal, trial: int, trail_data: np.ndarray):
+        return trail_data.mean() + (3.5 * trail_data.std())
+
+    measure_peaks(tdt_preprocessed_sessions, signal="Dopamine", include_detection_params=True, height=height_filter)
