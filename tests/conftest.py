80fe59926b95f8e6189ea7b16f666b147dd2b089
Josh Thackray <thackray@rutgers.edu>
2025-02-18 12:10:57 -0500

    add some more typings, abstract test data location

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,32 @@
 import multiprocessing
-import requests
-import zipfile
-import io
 import os
-import glob
 import pytest
 
-from fptools.io.data_loader import load_data
+from fptools.io.data_loader import load_data, SessionCollection
 from fptools.io.test import download_test_data
 from fptools.preprocess.pipelines import lowpass_dff
 
 
 @pytest.fixture(scope="session", autouse=True)
-def always_spawn():
+def always_spawn() -> None:
     """Force multiprocessing to always use the `spawn` start method.
 
-    Scope == 'session' so that it only runs once during testing.
+    scope == 'session' so that it only runs once during testing.
     autouse == True so that this fixture automatically runs at the start of testing.
+
+    This fixes some issues when running pytest and testing functions that use multiprocessing.
     """
     multiprocessing.set_start_method("spawn", force=True)
 
+def _get_test_data_location() -> str:
+    """Get the path to the location for testing data.
 
-def list_files(startpath):
+    Returns:
+        path to the test data location
+    """
+    return os.path.join(os.getcwd(), 'test_data')
+
+def list_files(startpath) -> None:
     """Helper method to print a directory tree with formatting.
 
     Args:
@@ -35,13 +40,13 @@ def list_files(startpath):
         for f in files:
             print('{}{}'.format(subindent, f))
 
-def pytest_sessionstart(session):
+def pytest_sessionstart(session) -> None:
     """Ensure test data is downloaded and extracted at the start of testing.
 
     Data will be downloaded as a zip file hosted on Box, and then unpacked to the local `test_data` folder
     in the root of the package.
     """
-    dest = download_test_data(os.path.join(os.getcwd(), 'test_data'))
+    dest = download_test_data(_get_test_data_location())
 
     print()
     print(f"Here are the contents of the test data folder \"{dest}\":")
@@ -51,16 +56,16 @@ def pytest_sessionstart(session):
 @pytest.fixture
 def tdt_test_data_path() -> str:
     """Get a path to a directory with TDT test data."""
-    return os.path.join(os.getcwd(), 'test_data', 'TDT-DLS-GRABDA2m-Male-PR4-2Day')
+    return os.path.join(_get_test_data_location(), 'TDT-DLS-GRABDA2m-Male-PR4-2Day')
 
 
 @pytest.fixture
 def ma_test_data_path() -> str:
     """Get a path to a directory with Med-Associates test data."""
-    return os.path.join(os.getcwd(), 'test_data', 'MA-PR4-4Day')
+    return os.path.join(_get_test_data_location(), 'MA-PR4-4Day')
 
 @pytest.fixture
-def tdt_preprocessed_sessions(tdt_test_data_path):
+def tdt_preprocessed_sessions(tdt_test_data_path) -> SessionCollection:
     """Fixture to provide some preprocessed TDT test data.
 
     lowpass_dff preprocessor will be run on the data.
@@ -94,7 +99,7 @@ def tdt_preprocessed_sessions(tdt_test_data_path):
 
 
 @pytest.fixture
-def ma_preprocessed_sessions(ma_test_data_path):
+def ma_preprocessed_sessions(ma_test_data_path) -> SessionCollection:
     """Fixture to provide some Med-Associates test data.
 
     Args:
