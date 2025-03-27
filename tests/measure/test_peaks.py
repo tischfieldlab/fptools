fdec68b02667863e4498b56ac22603f20a48c27e
Josh Thackray <thackray@rutgers.edu>
2025-03-27 17:45:45 -0400

    add test for detect_naive_peaks

--- tests/measure/test_peaks.py
+++ tests/measure/test_peaks.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 from fptools.io import Session, Signal
-from fptools.measure import measure_peaks
+from fptools.measure import measure_peaks, detect_naive_peaks, collect_signals
 
 
 def test_measure_peaks(tdt_preprocessed_sessions):
@@ -13,3 +13,9 @@ def test_measure_peaks_dynamic(tdt_preprocessed_sessions):
         return trail_data.mean() + (3.5 * trail_data.std())
 
     measure_peaks(tdt_preprocessed_sessions, signal="Dopamine", include_detection_params=True, height=height_filter)
+
+def test_detect_naive_peaks(tdt_preprocessed_sessions):
+    def collect_sigs(session:Session):
+        session.add_signal(collect_signals(session, signal="Dopamine", event="RNP"))
+    tdt_preprocessed_sessions.apply(collect_sigs)
+    detect_naive_peaks(tdt_preprocessed_sessions, signal="Dopamine@RNP", window=(0.0, 1.0))
